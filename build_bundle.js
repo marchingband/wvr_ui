bde45a8fc4852f3c049b787adae8f07164cae9ea
marchingband <youthclubrecords@gmail.com>
2021-12-14 16:25:58 -0800

    cleanup, write .gz to output just incase

--- build_bundle.js
+++ build_bundle.js
@@ -4,6 +4,9 @@ var pako = require('pako');
 fs.readFile('./output/bundle.min.js',(err, bundle)=>{
     if (err) throw err;
     const zip = pako.gzip(bundle)
+    fs.writeFile('./output/bundle.min.js.gz', zip,(err)=>{
+        console.log(err || "Successfully wrote bundle.min.js.gz to ./output" )
+    })
     var out = "#define BUNDLE_LEN " + zip.length + "\n" + "const uint8_t BUNDLE[] PROGMEM = {"
     zip.forEach(x=>{
         const char = Buffer.from([x]).toString('hex').toUpperCase()
